94a3caee04e7847a7939f9ad35b092556d9e0094
tenkelmann <t.enkelmann@web.de>
2019-08-04 16:00:19 +0200

    V1

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,9 @@
     #include <Wire.h>
     #include <NTPtimeESP.h>
     #include "cactus_io_BME280_I2C.h"
+
+    void handleNoRoot();
+
 // ************************************************************
 // Eingaben machen:
 // ************************************************************
@@ -68,6 +71,151 @@
 //Server
    ESP8266WebServer server(80);
 
+// -------------------------------------------------
+// Unterprogramm: IP Adresse ermitteln
+// -------------------------------------------------
+String myIp() {
+    char myIpString[24];
+    IPAddress myIp = WiFi.localIP();
+    sprintf(myIpString, "%d.%d.%d.%d", myIp[0], myIp[1], myIp[2], myIp[3]);
+    return myIpString;
+}
+// -------------------------------------------------
+// Unterprogramm: Webserver
+// -------------------------------------------------
+void handleRoot() { // bei Aufruf des Root-Verzeichnisses
+    String antwort = "";
+    int rssi = WiFi.RSSI();
+    antwort = "WeMos: \t\t" + Wemos_Device + "\n";
+    antwort = antwort + "==============================================\n\n";
+    antwort = antwort + "Stand: \t\t" + dayStr + "." + monatStr + "." + jahrStr + " um " + stdStr + ":" + minStr + ":" + sekStr + " Uhr\n";
+  // Ultraschall
+    antwort = antwort + "\n";
+    antwort = antwort + "Ultraschall:";
+    antwort = antwort + "\tDistance: \t\t" + distance + " cm\n";
+  // BME 280  
+    bme.readSensor(); 
+    antwort = antwort + "\n";
+    antwort = antwort + "BME 280:";
+    antwort = antwort + "\tAltitude: \t\t" + alti + " Meter\n";
+    antwort = antwort + "\t\tLuftdruck: \t\t" + (bme.getPressure_MB() + hPa_korrektur) + " hPa\n";
+    antwort = antwort + "\t\tLuftfeuchtigkeit: \t" + bme.getHumidity() + " %\n";
+    antwort = antwort + "\t\tTemperatur: \t\t" + bme.getTemperature_C() + " Grad Celsius\n";
+  // System
+    antwort = antwort + "\n";
+    antwort = antwort + "System:";
+    antwort = antwort + "\t\tVerbunden mit: \t\t" + ssid + "\n";
+    antwort = antwort + "\t\tSignalstaerke: \t\t" + String(rssi) + " dBm\n";
+    antwort = antwort + "\t\tIP-Adresse: \t\t" + myIp() + "\n";
+    antwort = antwort + "\n";
+  // MQTT
+    antwort = antwort + "ioB-MQTT:";
+    antwort = antwort + "\tName: \t\t\t" + mqtt_name + "\n";
+    antwort = antwort + "\t\tIP-Adresse: \t\t" + mqtt_ip + "\n";
+    antwort = antwort + "\t\tMQTT-Port: \t\t" + String(mqtt_port) + "\n";
+    antwort = antwort + "\t\tSende-Intervall: \t" + String(loop_mqtt/1000) + " Sekunden\n";
+    antwort = antwort + "\n\n";
+ 
+    antwort = antwort + "Folgende Werte einstellen mit: \t" + myIp() + "/wert?...\n";
+    antwort = antwort + "---------------------------------------------------------------------------------------\n";
+    antwort = antwort + "Sende-Intervall MQTT an iobroker\t" + "delay=x\t\t" + "(x = Sekunden)\n";
+    antwort = antwort + "Altitude des aktuellen Standorts\t" + "alti=x\t\t" + "(x = Meter ueber Meeresspiegel)\n";
+    antwort = antwort + "SSID fuer Wlan\t\t\t\t" + "ssid=x\t\t" + "(ssid zum einloggen)\n\n\n";
+
+    antwort = antwort + "@ 05/2019 bahnuhr, Dieter Mueller";
+    server.send(200, "text/plain", antwort);
+    delay(150);
+}
+// -------------------------------------------------
+// Unterprogramm: setup_wifi
+// -------------------------------------------------
+void setup_wifi() {
+  delay(10);
+// Connect WiFi
+  Serial.println(); Serial.println(); Serial.print("Connecting to "); Serial.println(ssid);
+  WiFi.hostname("Wemos_" + Wemos_Device);
+  WiFi.config(ip, gateway, subnet, dns1, dns2); WiFi.mode(WIFI_STA); WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500); Serial.print(".");
+  }
+  Serial.println(""); Serial.println(""); Serial.println("WiFi verbunden!");
+// Print the IP address
+  Serial.print("IP-Addresse: "); Serial.print(WiFi.localIP()); Serial.println("");
+}
+// -------------------------------------------------
+// Unterprogramm: setup_mqtt
+// -------------------------------------------------
+void setup_mqtt() {
+  client.begin(mqtt_ip, mqtt_port, net); // Verbindungsaufbau zu MQTT
+  Serial.println(); Serial.print("MQTT verbinden!");
+  while (!client.connect(mqtt_name, mqtt_user, mqtt_passwd)) { // Verbindung zum MQTT wird hergestellt
+    Serial.print(".");
+  }
+  Serial.println(); Serial.println("MQTT verbunden!"); Serial.println("");
+  client.publish("Wemos_" + Wemos_Device + "/IP-Adresse", myIp() );
+  client.publish("Wemos_" + Wemos_Device + "/Meldung", "WiFi und MQTT geprüft und ok.");
+}
+// -------------------------------------------------
+// Unterprogramm: setup_webserver
+// -------------------------------------------------
+void setup_webserver() {
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+  server.begin();
+  Serial.println("Webserver gestartet!");
+  Serial.print("Benutze diese URL: "); Serial.print("http://"); Serial.print(WiFi.localIP()); Serial.println("/"); Serial.println("");
+    // HTTP-Anfragen bearbeiten
+      server.on("/" , handleRoot);
+      server.onNotFound(handleNoRoot);
+}
+void handleNoRoot() {
+    String antwort = "";
+    if (server.args() > 0) {
+      for (uint8_t i = 0; i < server.args(); i++) {
+        if (server.argName(i) == "delay") {
+          loop_mqtt = ((server.arg(i)).toInt())*1000;
+          Serial.print("Delay eingestellt auf: "); Serial.print(loop_mqtt/1000); Serial.println(" Sekunden"); 
+          handleRoot();
+        } else if (server.argName(i) == "alti") {
+          alti = (server.arg(i)).toInt();
+          Serial.print("Altitude eingestellt auf: "); Serial.print(alti); Serial.println(" Meter ueber Meeresspiegel"); 
+          handleRoot();
+        } else if (server.argName(i) == "ssid") {
+          ssid = server.arg(i);
+          Serial.print("SSID eingestellt auf: "); Serial.println(ssid);  
+          setup_wifi();
+          setup_mqtt();
+          setup_webserver();
+          handleRoot();
+        } else {
+          server.send(404, "text/plain", "Eingabe ist falsch und konnte nicht bearbeitet werden!");
+        }
+      }
+    } else {
+      server.send(404, "text/plain", "Eingabe ist falsch und konnte nicht bearbeitet werden!");
+    }
+}
+// -------------------------------------------------
+// Unterprogramm: Time
+// -------------------------------------------------
+void Zeit_holen() {
+    dateTime = NTPch.getNTPtime(time_offset, time_summertime);
+    if(dateTime.valid){
+      //byte actualHour = dateTime.hour; byte actualMinute = dateTime.minute; byte actualsecond = dateTime.second;
+      //int actualyear = dateTime.year; byte actualMonth = dateTime.month; byte actualday =dateTime.day;
+      //byte actualdayofWeek = dateTime.dayofWeek;
+      jahrStr = dateTime.year;
+      monatStr = dateTime.month < 10 ? "0" + String(dateTime.month) : String(dateTime.month);
+      dayStr = dateTime.day < 10 ? "0" + String(dateTime.day) : String(dateTime.day);
+      stdStr = dateTime.hour < 10 ? "0" + String(dateTime.hour) : String(dateTime.hour);
+      minStr = dateTime.minute < 10 ? "0" + String(dateTime.minute) : String(dateTime.minute);
+      sekStr = dateTime.second < 10 ? "0" + String(dateTime.second) : String(dateTime.second);        
+      Serial.print("Stand: "); Serial.print(dayStr); Serial.print("."); Serial.print(monatStr); Serial.print("."); Serial.print(dateTime.year);
+      Serial.print(" um "); Serial.print(stdStr); Serial.print(":"); Serial.print(minStr); Serial.print(":"); Serial.print(sekStr); Serial.println(" Uhr");
+    }  
+}
 
 // *************************************************
 // Unterprogramm: void setup
@@ -172,151 +320,4 @@ void loop()  {
     }
 //Delay x before next reading
   delay(loop_delay);
-}
-
-
-// -------------------------------------------------
-// Unterprogramm: setup_wifi
-// -------------------------------------------------
-void setup_wifi() {
-  delay(10);
-// Connect WiFi
-  Serial.println(); Serial.println(); Serial.print("Connecting to "); Serial.println(ssid);
-  WiFi.hostname("Wemos_" + Wemos_Device);
-  WiFi.config(ip, gateway, subnet, dns1, dns2); WiFi.mode(WIFI_STA); WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500); Serial.print(".");
-  }
-  Serial.println(""); Serial.println(""); Serial.println("WiFi verbunden!");
-// Print the IP address
-  Serial.print("IP-Addresse: "); Serial.print(WiFi.localIP()); Serial.println("");
-}
-// -------------------------------------------------
-// Unterprogramm: setup_mqtt
-// -------------------------------------------------
-void setup_mqtt() {
-  client.begin(mqtt_ip, mqtt_port, net); // Verbindungsaufbau zu MQTT
-  Serial.println(); Serial.print("MQTT verbinden!");
-  while (!client.connect(mqtt_name, mqtt_user, mqtt_passwd)) { // Verbindung zum MQTT wird hergestellt
-    Serial.print(".");
-  }
-  Serial.println(); Serial.println("MQTT verbunden!"); Serial.println("");
-  client.publish("Wemos_" + Wemos_Device + "/IP-Adresse", myIp() );
-  client.publish("Wemos_" + Wemos_Device + "/Meldung", "WiFi und MQTT geprüft und ok.");
-}
-// -------------------------------------------------
-// Unterprogramm: setup_webserver
-// -------------------------------------------------
-void setup_webserver() {
-  if (MDNS.begin("esp8266")) {
-    Serial.println("MDNS responder started");
-  }
-  server.begin();
-  Serial.println("Webserver gestartet!");
-  Serial.print("Benutze diese URL: "); Serial.print("http://"); Serial.print(WiFi.localIP()); Serial.println("/"); Serial.println("");
-    // HTTP-Anfragen bearbeiten
-      server.on("/" , handleRoot);
-      server.onNotFound(handleNoRoot);
-}
-// -------------------------------------------------
-// Unterprogramm: IP Adresse ermitteln
-// -------------------------------------------------
-String myIp() {
-    char myIpString[24];
-    IPAddress myIp = WiFi.localIP();
-    sprintf(myIpString, "%d.%d.%d.%d", myIp[0], myIp[1], myIp[2], myIp[3]);
-    return myIpString;
-}
-// -------------------------------------------------
-// Unterprogramm: Time
-// -------------------------------------------------
-void Zeit_holen() {
-    dateTime = NTPch.getNTPtime(time_offset, time_summertime);
-    if(dateTime.valid){
-      byte actualHour = dateTime.hour; byte actualMinute = dateTime.minute; byte actualsecond = dateTime.second;
-      int actualyear = dateTime.year; byte actualMonth = dateTime.month; byte actualday =dateTime.day;
-      byte actualdayofWeek = dateTime.dayofWeek;
-      jahrStr = dateTime.year;
-      monatStr = dateTime.month < 10 ? "0" + String(dateTime.month) : String(dateTime.month);
-      dayStr = dateTime.day < 10 ? "0" + String(dateTime.day) : String(dateTime.day);
-      stdStr = dateTime.hour < 10 ? "0" + String(dateTime.hour) : String(dateTime.hour);
-      minStr = dateTime.minute < 10 ? "0" + String(dateTime.minute) : String(dateTime.minute);
-      sekStr = dateTime.second < 10 ? "0" + String(dateTime.second) : String(dateTime.second);        
-      Serial.print("Stand: "); Serial.print(dayStr); Serial.print("."); Serial.print(monatStr); Serial.print("."); Serial.print(dateTime.year);
-      Serial.print(" um "); Serial.print(stdStr); Serial.print(":"); Serial.print(minStr); Serial.print(":"); Serial.print(sekStr); Serial.println(" Uhr");
-    }  
-}
-// -------------------------------------------------
-// Unterprogramm: Webserver
-// -------------------------------------------------
-void handleRoot() { // bei Aufruf des Root-Verzeichnisses
-    String antwort = "";
-    int rssi = WiFi.RSSI();
-    antwort = "WeMos: \t\t" + Wemos_Device + "\n";
-    antwort = antwort + "==============================================\n\n";
-    antwort = antwort + "Stand: \t\t" + dayStr + "." + monatStr + "." + jahrStr + " um " + stdStr + ":" + minStr + ":" + sekStr + " Uhr\n";
-  // Ultraschall
-    antwort = antwort + "\n";
-    antwort = antwort + "Ultraschall:";
-    antwort = antwort + "\tDistance: \t\t" + distance + " cm\n";
-  // BME 280  
-    bme.readSensor(); 
-    antwort = antwort + "\n";
-    antwort = antwort + "BME 280:";
-    antwort = antwort + "\tAltitude: \t\t" + alti + " Meter\n";
-    antwort = antwort + "\t\tLuftdruck: \t\t" + (bme.getPressure_MB() + hPa_korrektur) + " hPa\n";
-    antwort = antwort + "\t\tLuftfeuchtigkeit: \t" + bme.getHumidity() + " %\n";
-    antwort = antwort + "\t\tTemperatur: \t\t" + bme.getTemperature_C() + " Grad Celsius\n";
-  // System
-    antwort = antwort + "\n";
-    antwort = antwort + "System:";
-    antwort = antwort + "\t\tVerbunden mit: \t\t" + ssid + "\n";
-    antwort = antwort + "\t\tSignalstaerke: \t\t" + String(rssi) + " dBm\n";
-    antwort = antwort + "\t\tIP-Adresse: \t\t" + myIp() + "\n";
-    antwort = antwort + "\n";
-  // MQTT
-    antwort = antwort + "ioB-MQTT:";
-    antwort = antwort + "\tName: \t\t\t" + mqtt_name + "\n";
-    antwort = antwort + "\t\tIP-Adresse: \t\t" + mqtt_ip + "\n";
-    antwort = antwort + "\t\tMQTT-Port: \t\t" + String(mqtt_port) + "\n";
-    antwort = antwort + "\t\tSende-Intervall: \t" + String(loop_mqtt/1000) + " Sekunden\n";
-    antwort = antwort + "\n\n";
- 
-    antwort = antwort + "Folgende Werte einstellen mit: \t" + myIp() + "/wert?...\n";
-    antwort = antwort + "---------------------------------------------------------------------------------------\n";
-    antwort = antwort + "Sende-Intervall MQTT an iobroker\t" + "delay=x\t\t" + "(x = Sekunden)\n";
-    antwort = antwort + "Altitude des aktuellen Standorts\t" + "alti=x\t\t" + "(x = Meter ueber Meeresspiegel)\n";
-    antwort = antwort + "SSID fuer Wlan\t\t\t\t" + "ssid=x\t\t" + "(ssid zum einloggen)\n\n\n";
-
-    antwort = antwort + "@ 05/2019 bahnuhr, Dieter Mueller";
-    server.send(200, "text/plain", antwort);
-    delay(150);
-}
-void handleNoRoot() {
-    String antwort = "";
-    if (server.args() > 0) {
-      for (uint8_t i = 0; i < server.args(); i++) {
-        if (server.argName(i) == "delay") {
-          loop_mqtt = ((server.arg(i)).toInt())*1000;
-          Serial.print("Delay eingestellt auf: "); Serial.print(loop_mqtt/1000); Serial.println(" Sekunden"); 
-          handleRoot();
-        } else if (server.argName(i) == "alti") {
-          alti = (server.arg(i)).toInt();
-          Serial.print("Altitude eingestellt auf: "); Serial.print(alti); Serial.println(" Meter ueber Meeresspiegel"); 
-          handleRoot();
-        } else if (server.argName(i) == "ssid") {
-          ssid = server.arg(i);
-          Serial.print("SSID eingestellt auf: "); Serial.println(ssid);  
-          setup_wifi();
-          setup_mqtt();
-          setup_webserver();
-          handleRoot();
-        } else {
-          server.send(404, "text/plain", "Eingabe ist falsch und konnte nicht bearbeitet werden!");
-        }
-      }
-    } else {
-      server.send(404, "text/plain", "Eingabe ist falsch und konnte nicht bearbeitet werden!");
-    }
 }
